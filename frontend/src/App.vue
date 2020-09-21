8f4bd961bdb71d31db49e2123c854c030a7a15bc
brian4462 <brian4462@gmail.com>
2020-09-21 17:13:36 +0900

    기초레이아웃

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,31 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
-    <router-view/>
+    <!--CONTAINER START-->
+    <b-container class="">
+      <!--HAEDER START-->
+      <div class="header">
+        <b-row class="subject">
+          <b-col>Lead By Daylight</b-col>
+        </b-row>
+        <b-row class="top_menu">
+          <b-col cols='2'></b-col>
+          <b-col>캐릭터</b-col>
+          <b-col>지도</b-col>
+          <b-col>공물</b-col>
+          <b-col>기능</b-col>
+          <b-col>공지</b-col>
+          <b-col cols='2'></b-col>
+        </b-row>
+      </div>
+      <!--HAEDER END-->
+      <!--ARTICLE START-->
+      <div class="article">
+        <router-view/>
+      </div>
+      <!--ARTICLE END-->
+    </b-container>
+    <!--CONTAINER END-->
   </div>
 </template>
 
@@ -12,12 +36,21 @@ export default {
 </script>
 
 <style>
+html, body{
+  height: 100%; margin: 0; padding: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color:cornsilk;
+  background-color:#2c3e50;
+  min-height: 100%;
+}
+
+.header .col {
+  padding: 20px;
 }
 </style>
