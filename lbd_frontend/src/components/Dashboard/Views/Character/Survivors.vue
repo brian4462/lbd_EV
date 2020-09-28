a5a56c1b5369baa667c71a661fd5ba9f4a15bde5
brian4462 <brian4462@gmail.com>
2020-09-28 10:14:47 +0900

    생존자 카드 클릭시 영역이 겹치던 문제 해결

--- lbd_frontend/src/components/Dashboard/Views/Character/Survivors.vue
+++ lbd_frontend/src/components/Dashboard/Views/Character/Survivors.vue
@@ -2,7 +2,7 @@
   <div>
     <!--Stats cards-->
     <div class="row">
-      <div :class="[selected==survivors.surv_id?'col-12':'col-lg-3 col-md-4 col-sm-6 ex_custom',selected==survivors.surv_id?'active':'']" @click="toggleActive(survivors.surv_id)" v-for="survivors in survivorsCards"  :id ="'div_'+survivors.surv_id" :ref="survivors.surv_id" :key="survivors.surv_id">
+      <div :class="[selected==survivors.surv_id?'col-lg-12 col-md-12 col-sm-12 ex_custom':'col-lg-3 col-md-4 col-sm-6 ex_custom',selected==survivors.surv_id?'active':'']" @click="toggleActive(survivors.surv_id)" v-for="survivors in survivorsCards"  :id ="'div_'+survivors.surv_id" :ref="survivors.surv_id" :key="survivors.surv_id">
         <stats-card>
             <div slot="header">
               <img style="max-width:120px;max-height:170px;" :src="survivors.photo_path">
@@ -11,7 +11,7 @@
               <p>{{survivors.surv_nm_ko}}</p>
               {{survivors.surv_nm_en}}
             </div>
-            <div class="body" slot="body">
+            <div class="body" slot="body" v-show="selected==survivors.surv_id?true:false">
               Chart data used to render stats, charts. Should be replaced with server data
             </div>
         </stats-card>
@@ -62,15 +62,6 @@
 <style>
   .ex_custom {
     transition:all .3s ease;
-  }
-  .ex_custom:active {
-    position:static;
-  }
-  .ex_custom .body {
-    display: none;
-  }
-  .ex_custom:active .body{
-    display: block;
   }
 
 </style>
